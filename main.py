4b1c85787b8660e91043ee016dc4fabeb2be8e36
Brian Ho <47989819+briannho@users.noreply.github.com>
2019-12-05 10:00:26 -0600

    Update main.py
    Added other classifiers

--- main.py
+++ main.py
@@ -7,62 +7,49 @@ Created on Wed Nov 13 10:59:44 2019
 """
 
 #################
-#Loading the Data
+#PROCESSING THE DATA
 #################
 import pandas as pd
 import numpy as np
-data = pd.read_csv('risk_factors_cervical_cancer.csv', delimiter=',')
+data = pd.read_csv('risk_factors_cervical_cancer.csv', delimiter = ',')
 
+#Remove Unwanted Data
 data = data.replace('?', np.nan)
-data = data.drop(['STDs: Time since last diagnosis', 'STDs: Time since first diagnosis', 'Schiller', 'Hinselmann', 'Citology'], axis=1)
+data = data.drop(['STDs: Time since last diagnosis', 'STDs: Time since first diagnosis', 'Schiller', 'Hinselmann', 'Citology'], axis = 1)
 data = data.dropna()
 
-#################
 #Splitting the Data
-#################
-#np.random.seed(2019)
+#np.random.seed(479)
 from sklearn.model_selection import train_test_split
-train, test = train_test_split(data, test_size=0.25)
+train, test = train_test_split(data, test_size = 0.25)
 
 x_train = train.iloc[:,:-1].values
 y_train = train['Biopsy'].values
 x_test = test.iloc[:,:-1].values
 y_test = test['Biopsy'].values
 
-#################
 #Standardizing the Data
-#################
 from sklearn.preprocessing import StandardScaler
 
 ss = StandardScaler() 
 x_train = ss.fit_transform(x_train)
 x_test = ss.transform(x_test)
 
-#convert from np arrays back to dataframes
+#Convert From np Arrays Back to Dataframes
 x_train = pd.DataFrame(x_train)
 x_test = pd.DataFrame(x_test)
 
 #################
-#Feature Selection
+#FEATURE SELECTION
 #################
-#ANOVA
-#import sklearn.feature_selection as fs
-#features = fs.f_classif(x_train, y_train)
-#print(features[1])
-
-#backward selection
+print('The data has been loaded and processed.\n')
 from sklearn.linear_model import LogisticRegression
 
-#lr = LogisticRegression(solver='liblinear') #saga
-#rfe_lr = fs.RFE(estimator = lr)
-#results = rfe_lr.fit(x_train, y_train).ranking_
-#print(results)
-
-#Lasso i.e. l1 regularization
-lr_fs = LogisticRegression(solver='liblinear', penalty = 'l1') #saga
+#Lasso i.e. l1 Regularization
+lr_fs = LogisticRegression(solver='liblinear', penalty = 'l1')
 lr_fs.fit(x_train, y_train)
 
-#get coefficients 
+#Get Coefficients 
 all_features = lr_fs.coef_[0,:].tolist()
 
 index = -1
@@ -70,18 +57,27 @@ for feature in all_features:
 
     if feature == 0: #removes predictor from training set if coefficient is 0
         index = all_features.index(feature, index+1)
-        #print(index)
         x_train.drop([index], axis = 1, inplace = True)
         x_test.drop([index], axis = 1, inplace = True)
     else:
         next
 
+#Print Out the Best Features
+selected_features = []
+for col in x_train.columns:
+    selected_features.append(train.columns[col])
+    
+print('The best features are: ' + ', '.join(selected_features) + '.')
+
+#convert Dataframe to Array for Model Training
 x_train = x_train.values
         
-        
 #################
+#LOGISTIC REGRESSION
+#################
+print('\nLOGISTIC REGRESSION')
+
 #Grid Search
-#################
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import accuracy_score
 
@@ -90,79 +86,160 @@ best_c = -1000
 C = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000, 100000]
 kfolds = StratifiedKFold(n_splits=10) #shuffle and randomstate??
 
-#testing different values for C
+#Testing Different Values for C
 for c in C:
     predicted = []
-    #scores = []
-    lr_gs = LogisticRegression(solver='liblinear', C=c) #saga
+    lr_gs = LogisticRegression(solver = 'liblinear', C = c)
     
-    #10-fold cross validation
-    for train, test in kfolds.split(x_train, y_train):
-        lr_gs.fit(x_train[train], y_train[train])
-        pred = lr_gs.predict(x_train[test])
+    #10-Fold Cross Validation
+    for training, testing in kfolds.split(x_train, y_train):
+        lr_gs.fit(x_train[training], y_train[training])
+        pred = lr_gs.predict(x_train[testing])
+        
         for p in pred:
             predicted.append(p)
-        #score = lr_gs.score(x_train[test], y_train[test])
-        #scores.append(score)
     
     score = accuracy_score(y_train, predicted)
+    
+    if score > CV_acc:
+        CV_acc = score
+        best_c = c     
+
+print('The best C is ' + str(best_c) + '.')
+print('The training accuracy is: ' + str(CV_acc) + '.')
+
+#Test Set Performance
+from sklearn.metrics import confusion_matrix
+
+final_lr = LogisticRegression(solver = 'liblinear', C = best_c)
+final_lr.fit(x_train, y_train)
+lr_predicted = final_lr.predict(x_test)
+lr_acc = accuracy_score(y_test, lr_predicted)
+
+print('The test accuracy is: ' + str(lr_acc) + '.')
+print(confusion_matrix(y_test, lr_predicted))
+
+#################
+#SUPPORT VECTOR MACHINE
+#################
+print('\nSUPPORT VECTOR MACHINE')
+from sklearn.svm import SVC
+
+#Grid Search
+CV_acc = 0
+best_c = -1000
+for c in C:
+    predicted = []
+    svm_gs = SVC(gamma = 'auto', C = c)
+    
+    #10-Fold Cross Validation
+    for training, testing in kfolds.split(x_train, y_train):
+        svm_gs.fit(x_train[training], y_train[training])
+        pred = svm_gs.predict(x_train[testing])
+        
+        for p in pred:
+            predicted.append(p)
+    
+    score = accuracy_score(y_train, predicted)
+    
     if score > CV_acc:
         CV_acc = score
         best_c = c     
     
-#    if np.mean(scores) > CV_acc:
-#        CV_acc = np.mean(scores)
-#        best_c = c
+print('The best C is ' + str(best_c) + '.')
+print('The training accuracy is: ' + str(CV_acc) + '.')
+
+#Test Set Performance
+final_svm = SVC(gamma = 'auto', C = best_c)
+final_svm.fit(x_train, y_train)
+svm_predicted = final_svm.predict(x_test)
+svm_acc = accuracy_score(y_test, svm_predicted)
+print('The test accuracy is: ' + str(svm_acc) + '.')
+print(confusion_matrix(y_test, svm_predicted))
+
+#################
+#DECISION TREE
+#################
+print('\nCLASSIFICATION DECISION TREE')
+from sklearn.tree import DecisionTreeClassifier
+
+#Train Classifier
+tree = DecisionTreeClassifier()
+tree.fit(x_train, y_train)
+
+#Test Set Performance
+tree_predicted = tree.predict(x_test)
+tree_acc = accuracy_score(y_test, tree_predicted)
+print('The test accuracy is: ' + str(tree_acc) + '.')
+print(confusion_matrix(y_test, tree_predicted))
+
+#################
+#BRANDOM FOREST
+#################
+print('\nRANDOM FOREST')
+from sklearn.ensemble import RandomForestClassifier
+
+#Train Classifer
+rf = RandomForestClassifier(n_estimators = 100)
+rf.fit(x_train, y_train)
+
+#Test Set Performance
+rf_predicted = rf.predict(x_test)
+rf_acc = accuracy_score(y_test, rf_predicted)
+print('The test accuracy is: ' + str(rf_acc) + '.')
+print(confusion_matrix(y_test, rf_predicted))
+
+#################
+#Building KNN
+#################
+print('\nK NEAREST NEIGHBORS')
+from sklearn.neighbors import KNeighborsClassifier
+
+#Train Classifier
+knn = KNeighborsClassifier()
+knn.fit(x_train, y_train)
+
+#Test Set Performance
+predicted = knn.predict(x_test)
+knn_acc = accuracy_score(y_test, predicted)
+print('The test accuracy is: ' + str(knn_acc) + '.')
+print(confusion_matrix(y_test, predicted))
+
+#################
+#Building LDA
+#################
+print('\nLINEAR DISCRIMINANT ANALYSIS')
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+#Grid Search
+CV_acc = 0
+best_s = -1000
+shrinkage = np.linspace(0.0, 1.0, 20)
+for s in shrinkage:
+    predicted = []
+    lda_gs = LinearDiscriminantAnalysis(solver = 'lsqr', shrinkage = s)
     
+    #10-fold cross validation
+    for training, testing in kfolds.split(x_train, y_train):
+        lda_gs.fit(x_train[training], y_train[training])
+        pred = lda_gs.predict(x_train[testing])
+        
+        for p in pred:
+            predicted.append(p)
     
-print('The best C is ' + str(best_c))
+    score = accuracy_score(y_train, predicted)
+    
+    if score > CV_acc:
+        CV_acc = score
+        best_s = s    
 
-#################
-#building the final LR model
-#################
-final_model = LogisticRegression(solver='liblinear', C=best_c) #saga
-final_model.fit(x_train, y_train)
+print('The best shrinkage parameter is ' + str(best_s) + '.')
+print('The training accuracy is: ' + str(CV_acc) + '.')
 
-#################
-#performing on the test set
-#################
-predicted = final_model.predict(x_test)
-#lr_acc = model.score(x_test, y_test)
-lr_acc = accuracy_score(y_test, predicted)
-print(lr_acc)
-
-##building SVM
-#from sklearn.svm import SVC
-#svm = SVC()
-#svm.fit(train.iloc[:,:-1 ], train['Biopsy'])
-#svm_acc = svm.score(test.iloc[:,:-1 ], test['Biopsy'])
-#print(svm_acc)
-#
-##building decision tree
-#from sklearn.tree import DecisionTreeClassifier
-#tree = DecisionTreeClassifier()
-#tree.fit(train.iloc[:,:-1 ], train['Biopsy'])
-#tree_acc = tree.score(test.iloc[:,:-1 ], test['Biopsy'])
-#print(tree_acc)
-#
-##building random forest
-#from sklearn.ensemble import RandomForestClassifier
-#rf = RandomForestClassifier()
-#rf.fit(train.iloc[:,:-1 ], train['Biopsy'])
-#rf_acc = rf.score(test.iloc[:,:-1 ], test['Biopsy'])
-#print(rf_acc)
-#
-##building KNN
-#from sklearn.neighbors import KNeighborsClassifier
-#knn = KNeighborsClassifier()
-#knn.fit(train.iloc[:,:-1 ], train['Biopsy'])
-#knn_acc = knn.score(test.iloc[:,:-1 ], test['Biopsy'])
-#print(knn_acc)
-#
-##building LDA
-#from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-#lda = LinearDiscriminantAnalysis()
-#lda.fit(train.iloc[:,:-1 ], train['Biopsy'])
-#lda_acc = lda.score(test.iloc[:,:-1 ], test['Biopsy'])
-#print(lda_acc)
-
+#Test Set Performance
+final_lda = LinearDiscriminantAnalysis(solver = 'lsqr', shrinkage = best_s)
+final_lda.fit(x_train, y_train)
+lda_predicted = final_lda.predict(x_test)
+lda_acc = accuracy_score(y_test, lda_predicted)
+print('The test accuracy is: ' + str(lda_acc) + '.')
+print(confusion_matrix(y_test, lda_predicted))
